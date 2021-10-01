e0f037ba74cc0e4e3381fca6686c8930aa0d5834
R Williams <rjhockey1313@gmail.com>
2021-10-01 15:29:27 -0400

    Platform
    Added beach towel as platform. Updated jump code and revised player collider radius so isgrounded fires off correctly.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,36 +1,4 @@
-//using UnityEngine;
-
-//public class CharacterMovement : MonoBehaviour
-//{
-//    [SerializeField] private float characterMoveSpeed;
-//    public float jumpSpeed = 8f;
-//    private float dirX;
-//    private Rigidbody2D rb;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        //characterMoveSpeed = 5f;
-//        rb = GetComponent<Rigidbody2D>();
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        // determine left or right keyboard input
-//        dirX = Input.GetAxis("Horizontal") * characterMoveSpeed * Time.deltaTime;
-//        // moves player to left or right
-//        transform.position = new Vector2(transform.position.x + dirX, transform.position.y);
-
-//        if (Input.GetButtonDown("Jump"))
-//        {
-//            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-//        }
-//    }
-//}
-
 using UnityEngine;
-using System.Collections;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -38,6 +6,11 @@ public class CharacterMovement : MonoBehaviour
     public float jumpSpeed = 8f;
     private float movement = 0f;
     private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius; // equal to radius of the player collider
+    public LayerMask groundLayer;
+    [SerializeField] private bool isTouchingGround;
+
     // Use this for initialization
     void Start()
     {
@@ -47,22 +20,29 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        
         movement = Input.GetAxis("Horizontal");
+
         if (movement > 0f)
         {
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
         }
+
         else if (movement < 0f)
         {
             rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
         }
+
         else
         {
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+            isTouchingGround = false;
         }
     }
 }
